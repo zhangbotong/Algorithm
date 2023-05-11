2079f974a238fb9584378df8e22c17d506373df7
Kyrie <zhanghang004@gmail.com>
2023-05-11 13:55:20 +0800

    add recursion and loo

---
+++ src/main/java/backtracking/note/Recursion and loop.md
@@ -0,0 +1,10 @@
+### 递归（Recursion，DFS）
+
+组成：base case + recursive call
+
+base case：相当于 Loop 中的循环终止条件。在递归中就是深度遍历到了一条最深处。
+
+适用：当不知道有几层循环，只知道几层循环的条件时，如：
+
+### 循环（Loop）
+
--- src/main/java/dp/_08_01KnapsackTwoDim.java
+++ src/main/java/dp/_08_01KnapsackTwoDim.java
@@ -17,7 +17,7 @@ import java.util.Arrays;
 public class _08_01KnapsackTwoDim {
 
     /**
-     * 01 背包，二维数组解法，先遍历背包容量，再遍历物品，符合自然思维
+     * 01 背包，二维数组解法，先遍历背包容量，再遍历物品，符合自然思维。其中，物品从前往后，背包也从前往后
      */
     public int knapsack01TwoDimBagFirst(int[] w, int[] v, int bagSize) {
         int goodsCount = w.length - 1;
@@ -38,7 +38,7 @@ public class _08_01KnapsackTwoDim {
     }
 
     /**
-     * 01 背包，二维数组解法，先遍历物品，再遍历背包容量
+     * 01 背包，二维数组解法，先遍历物品，再遍历背包容量，物品从前往后，背包也从前往后
      */
     public int knapsack01TwoDimGoodsFirst(int[] w, int[] v, int bagSize) {
         int goodsCount = w.length - 1;
@@ -59,6 +59,27 @@ public class _08_01KnapsackTwoDim {
         return dp[goodsCount - 1][bagSize];
     }
 
+    /**
+     * 01 背包，二维数组解法，先遍历物品，再遍历背包容量，物品从前往后，背包容量从后往前
+     */
+    public int knapsack01TwoDimGoodsFirstKnapsackInverse (int[] w, int[] v, int bagSize){
+        int goodsCount = w.length - 1;
+        int[][] dp = new int[goodsCount + 1][bagSize + 1];// 自带初始化第一行、第一列为 0
+        for (int i = 1; i <= goodsCount; i++) {
+            for (int b = bagSize; b >= 1; b--) {
+                if (b - w[i] < 0) {
+                    dp[i][b] = dp[i - 1][b];
+                } else {
+                    int notPutI = dp[i - 1][b];
+                    int putI = v[i] + dp[i - 1][b - w[i]];
+                    dp[i][b] = Math.max(notPutI, putI);
+                }
+            }
+        }
+        System.out.println(Arrays.deepToString(dp).replace("], ", "]\n"));
+        return dp[goodsCount][bagSize];
+    }
+
     public int knapsack01OneDim(int[] w, int[] v, int bagSize) {
         int goodsCount = w.length - 1;
         int[] dp = new int[bagSize + 1];
@@ -77,6 +98,6 @@ public class _08_01KnapsackTwoDim {
     public void test() {
         int[] weight = {0, 1, 2, 4};
         int[] value = {0, 15, 20, 30};
-        System.out.println(knapsack01OneDim(weight, value, 4));
+        System.out.println(knapsack01TwoDimGoodsFirstKnapsackInverse(weight, value, 4));
     }
 }
